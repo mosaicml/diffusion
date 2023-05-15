77cb95af8a48850db0fc43a31dfe1d7169a3c096
Cory Stephenson <cory@mosaicml.com>
2023-05-15 17:37:26 +0000

    Add option to use different scheduler functions

--- diffusion/schedulers/schedulers.py
+++ diffusion/schedulers/schedulers.py
@@ -7,6 +7,22 @@ import numpy as np
 import torch
 
 
+def tangent_schedule(times):
+    """Computes beta(t) sin(phi(t)) and cos(phi(t)).
+
+    Implements a schedule where angle = time, hence beta = 2 * tan(t).
+    """
+    if isinstance(times, torch.Tensor):
+        beta_t = 2 * torch.tan(times)
+        cos_phi_t = torch.cos(times)
+        sin_phi_t = torch.sin(times)
+    else:
+        beta_t = 2 * np.tan(times)
+        cos_phi_t = np.cos(times)
+        sin_phi_t = np.sin(times)
+    return beta_t, sin_phi_t, cos_phi_t
+
+
 class ContinuousTimeScheduler:
     """Scheduler for continuous time (variance preserving) diffusion models.
 
@@ -15,22 +31,27 @@ class ContinuousTimeScheduler:
     zero can occur.
 
     Args:
-        t_max (float): The maximum timestep in the diffusion process. Defaults to 1.57.
-        num_inference_timesteps (int): The number of timesteps to use during inference. Defaults to 50.
+        t_max (float): The maximum timestep in the diffusion process. Default: `1.57`.
+        num_inference_timesteps (int): The number of timesteps to use during inference. Default `50`.
         prediction_type (str): The type of prediction to use during inference. Must be one of 'sample', 'epsilon', or
-            'v_prediction'. Defaults to 'epsilon'.
-        use_ode (bool): Whether to use Euler's method to integrate the probability flow ODE. Defaults to False.
+            'v_prediction'. Default: `epsilon`.
+        use_ode (bool): Whether to use Euler's method to integrate the probability flow ODE. Default: `False`.
+        schedule_function (callable): A function that takes in a tensor of times and returns beta(t), sin(phi(t)),
+            cos(phi(t)). Default: `tangent_schedule`.
     """
 
     def __init__(self,
                  t_max: float = 1.57,
                  num_inference_timesteps: int = 50,
                  prediction_type: str = 'epsilon',
-                 use_ode: bool = False):
+                 use_ode: bool = False,
+                 schedule_function=tangent_schedule):
         self.t_max = t_max
         self.num_inference_timesteps = num_inference_timesteps
         self.prediction_type = prediction_type
         self.use_ode = use_ode
+        self.schedule_function = schedule_function
+
         self.timesteps = np.linspace(self.t_max, 0, num=num_inference_timesteps, endpoint=False)
         self.init_noise_sigma = 1.0  # Needed to work with our generate function that uses huggingface schedulers
 
@@ -46,21 +67,18 @@ class ContinuousTimeScheduler:
         while len(timesteps.shape) < len(inputs.shape):
             timesteps = timesteps.unsqueeze(-1)
         # compute sin, cos of the angle
-        # A more general implementation would first compute the angle from the timesteps
-        sin_t = torch.sin(timesteps)
-        cos_t = torch.cos(timesteps)
+        _, sin_phi, cos_phi = self.schedule_function(timesteps)
         # combine the signal with the noise
-        return cos_t * inputs + sin_t * noise
+        return cos_phi * inputs + sin_phi * noise
 
     def get_velocity(self, inputs, noise, timesteps):
         # v is defined by -sin(t) * inputs + cos(t) * noise
         # expand timesteps to the right number of dimensions
         while len(timesteps.shape) < len(inputs.shape):
             timesteps = timesteps.unsqueeze(-1)
-        # A more general implementation would first compute the angle from the timesteps
-        sin_t = torch.sin(timesteps)
-        cos_t = torch.cos(timesteps)
-        return -sin_t * inputs + cos_t * noise
+        # compute sin, cos of the angle
+        _, sin_phi, cos_phi = self.schedule_function(timesteps)
+        return -sin_phi * inputs + cos_phi * noise
 
     def scale_model_input(self, model_input, t):
         # Needed to work with our generate function that uses huggingface schedulers
@@ -69,14 +87,8 @@ class ContinuousTimeScheduler:
     def step(self, model_output, t, model_input, generator=None):
         if t == 0:
             return {'prev_sample': model_input}
-        # Compute beta at the current timestep
-        tan_t = np.tan(t)
-        beta_t = 2 * tan_t
-        # compute sin, cos, tan of the angle
-        # A more general implementation would first calculate an angle from beta_t and the timestep
-        # Then compute sin, cos.
-        sin_t = np.sin(t)
-        cos_t = np.cos(t)
+        # compute beta, sin, cos
+        beta_t, sin_phi_t, cos_phi_t = self.schedule_function(t)
         # Compute the time deltas
         # A more general implementation would allow for unequally spaced timesteps.
         dt = self.t_max / self.timesteps.shape[0]
@@ -84,14 +96,14 @@ class ContinuousTimeScheduler:
         if self.prediction_type == 'sample':
             x_0 = model_output
         elif self.prediction_type == 'epsilon':
-            x_0 = (model_input - sin_t * model_output) / cos_t
+            x_0 = (model_input - sin_phi_t * model_output) / cos_phi_t
         elif self.prediction_type == 'v_prediction':
-            x_0 = cos_t * model_input - sin_t * model_output
+            x_0 = cos_phi_t * model_input - sin_phi_t * model_output
         else:
             raise ValueError(
                 f'prediction type must be one of sample, epsilon, or v_prediction. Got {self.prediction_type}')
         # Compute the score function
-        score = -(model_input - cos_t * x_0) / np.square(sin_t)
+        score = -(model_input - cos_phi_t * x_0) / np.square(sin_phi_t)
         # Compute the previous sample x_t -> x_t-1
         if self.use_ode:
             # Use Euler's method to integrate the probability flow ODE
