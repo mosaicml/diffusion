5de927e466b52c39b3dc682df9ae9a73ace64cbe
Cory Stephenson <cory@mosaicml.com>
2023-05-15 20:17:18 +0000

    Add pixel models

--- diffusion/models/__init__.py
+++ diffusion/models/__init__.py
@@ -3,10 +3,14 @@
 
 """Diffusion models."""
 
-from diffusion.models.models import stable_diffusion_2
+from diffusion.models.models import continuous_pixel_diffusion, discrete_pixel_diffusion, stable_diffusion_2
+from diffusion.models.pixel_diffusion import PixelDiffusion
 from diffusion.models.stable_diffusion import StableDiffusion
 
 __all__ = [
+    'continuous_pixel_diffusion',
+    'discrete_pixel_diffusion',
+    'PixelDiffusion',
     'stable_diffusion_2',
     'StableDiffusion',
 ]
