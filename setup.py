a26e5e5273d51615fb11ea8bccbad6d3209a7162
Cory Stephenson <cory.stephenson@databricks.com>
2023-08-09 19:28:39 +0000

    Update reqs for h100s

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@
 from setuptools import find_packages, setup
 
 install_requires = [
-    'mosaicml@git+https://github.com/mosaicml/composer.git@6cf3d3a1aa300834c650f89460b5ac9bbc5a1e46',
+    'mosaicml',
     'mosaicml-streaming>=0.4.0,<1.0',
     'hydra-core>=1.2',
     'hydra-colorlog>=1.1.0',
-    'diffusers[torch]==0.16.0',
-    'transformers[torch]==4.29.2',
+    'diffusers[torch]==0.19.3',
+    'transformers[torch]==4.31.0',
     'wandb==0.15.4',
-    'xformers==0.0.16',
+    'xformers==0.0.20',
     'triton==2.0.0',
-    'torchmetrics[image]==0.11.3',
+    'torchmetrics[image]==0.11.4',
     'clean-fid',
     'clip@git+https://github.com/openai/CLIP.git',
 ]
