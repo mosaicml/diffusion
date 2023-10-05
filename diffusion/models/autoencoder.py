24372d62f6dc12140091ac382b2996c04a66d32f
Cory Stephenson <cory.stephenson@databricks.com>
2023-10-05 04:04:15 +0000

    Fix discriminator inits as well

--- diffusion/models/autoencoder.py
+++ diffusion/models/autoencoder.py
@@ -501,23 +501,29 @@ class NlayerDiscriminator(nn.Module):
         self.num_layers = num_layers
 
         self.blocks = nn.ModuleList()
-        self.blocks.append(nn.Conv2d(self.input_channels, self.num_filters, kernel_size=4, stride=2, padding=1))
+        input_conv = nn.Conv2d(self.input_channels, self.num_filters, kernel_size=4, stride=2, padding=1)
+        nn.init.kaiming_normal_(input_conv.weight, nonlinearity='linear')
+        self.blocks.append(input_conv)
+
         out_filters = self.num_filters
         for n in range(1, self.num_layers):
             in_filters = self.num_filters * 2**(n - 1)
             out_filters = self.num_filters * min(2**n, 8)
             conv = nn.Conv2d(in_filters, out_filters, kernel_size=4, stride=2, padding=1, bias=False)
+            nn.init.kaiming_normal_(conv.weight, nonlinearity='leaky_relu', a=0.2)
             norm = nn.BatchNorm2d(out_filters)
             nonlinearity = nn.LeakyReLU(0.2, True)
             self.blocks.extend([conv, norm, nonlinearity])
         # Make the output layers
         final_out_filters = self.num_filters * min(2**self.num_layers, 8)
         conv = nn.Conv2d(out_filters, final_out_filters, kernel_size=4, stride=1, padding=1, bias=False)
+        nn.init.kaiming_normal_(conv.weight, nonlinearity='leaky_relu', a=0.2)
         norm = nn.BatchNorm2d(final_out_filters)
         nonlinearity = nn.LeakyReLU(0.2, True)
         self.blocks.extend([conv, norm, nonlinearity])
         # Output layer
         output_conv = nn.Conv2d(final_out_filters, 1, kernel_size=4, stride=1, padding=1, bias=False)
+        nn.init.kaiming_normal_(output_conv.weight, nonlinearity='linear')
         self.blocks.append(output_conv)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
