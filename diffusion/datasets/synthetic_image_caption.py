96d2967a232298b315faddc2f0c1520dea514341
Landan Seguin <landan@mosaicml.com>
2023-10-06 10:37:51 -0700

    Don't waste memory on samples

--- diffusion/datasets/synthetic_image_caption.py
+++ diffusion/datasets/synthetic_image_caption.py
@@ -19,17 +19,12 @@ class SyntheticImageCaptionDataset(Dataset):
         num_samples (int): Number of samples in the synthetic dataset. Default: ``100_000``.
     """
 
-    def __init__(self, image_size: int = 512, caption_length: int = 77, num_samples: int = 100_000, sdxl: bool = False):
+    def __init__(self, image_size: int = 512, caption_length: int = 77, sdxl: bool = False):
 
         super().__init__()
         self.image_size = image_size
-        self.num_samples = num_samples
         self.sdxl = sdxl
-
-        self.images = torch.randn(num_samples, 3, image_size, image_size)
-        caption_shape = (num_samples, 2, caption_length) if self.sdxl else (num_samples, caption_length)
-        self.captions = torch.randint(0, 128, caption_shape, dtype=torch.long)
-
+        self.caption_shape = (2, caption_length) if self.sdxl else (caption_length)
 
     def __len__(self):
         return len(self.images)
@@ -40,8 +35,8 @@ class SyntheticImageCaptionDataset(Dataset):
             out['cond_crops_coords_top_left'] = torch.tensor([0, 0])
             out['cond_original_size'] = torch.tensor([self.image_size, self.image_size])
             out['cond_target_size'] = torch.tensor([self.image_size, self.image_size])
-        out['image'] = self.images[idx]
-        out['captions'] = self.captions[idx]
+        out['image'] = torch.randn(3, self.image_size, self.image_size)
+        out['captions'] = torch.randint(0, 128, self.caption_shape, dtype=torch.long)
         return out
 
 
@@ -49,7 +44,6 @@ def build_synthetic_image_caption_dataloader(
     batch_size: int,
     image_size: int = 512,
     caption_length: int = 77,
-    num_samples: int = 100_000,
     sdxl: bool = False,
     dataloader_kwargs: Optional[Dict] = None,
 ):
@@ -68,7 +62,6 @@ def build_synthetic_image_caption_dataloader(
     dataset = SyntheticImageCaptionDataset(
         image_size=image_size,
         caption_length=caption_length,
-        num_samples=num_samples,
         sdxl=sdxl,
     )
 
