bc506ef3de11e81b7edae9dbbb8280f4ccd9af92
austin <jacobsonaustinj@gmail.com>
2023-11-22 03:37:24 -0800

    add tests for sdxl foward and sdxl_e5 forward

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,7 +7,7 @@
 import pytest
 import torch
 
-from diffusion.models.models import stable_diffusion_2
+from diffusion.models.models import stable_diffusion_2, stable_diffusion_xl
 
 
 def test_model_forward():
@@ -44,3 +44,26 @@ def test_model_generate(guidance_scale, negative_prompt):
         progress_bar=False,
     )
     assert output.shape == (1, 3, 8, 8)
+
+@pytest.mark.parametrize('use_e5', [True, False])
+def test_model_forward(use_e5):
+    model = stable_diffusion_xl(use_e5=use_e5, pretrained=False, fsdp=False, encode_latents_in_fp16=False, clip_qkv=None)
+    batch_size = 1
+    H = 32
+    W = 32
+    image = torch.randn(batch_size, 3, H, W)
+    latent = torch.randn(batch_size, 4, H // 8, W // 8)
+    caption = torch.randint(low=0, high=128, size=(
+        batch_size,
+        77,
+    ), dtype=torch.long)
+    randfloat = torch.randn(batch_size, 1)
+    caption = torch.stack([caption, caption], dim=1)
+    batch = {'image': image,
+            'captions': caption,
+            'cond_original_size': randfloat,
+            'cond_crops_coords_top_left': randfloat,
+            'cond_target_size': randfloat}
+    output, target, _ = model(batch)  # model.forward generates the unet output noise or v_pred target.
+    assert output.shape == latent.shape
+    assert target.shape == latent.shape
