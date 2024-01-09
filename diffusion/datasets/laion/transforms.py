90b97ed93e42cde98bc536501fd9ef16bb9a9ced
Landan Seguin <landanjs@gmail.com>
2024-01-09 12:27:39 -0800

    Remove rounding in aspect ratio bucketing transform (#111)
    Remove rounding for aspect ratio buckets

--- diffusion/datasets/laion/transforms.py
+++ diffusion/datasets/laion/transforms.py
@@ -58,8 +58,7 @@ class RandomCropAspectRatioTransorm:
             1088, 1024, 1024, 960, 960, 896, 896, 832, 832, 768, 768, 704, 704, 640, 640, 576, 576, 576, 512, 512, 512,
             512
         ])
-        # torch.round is a temporarily needed due to an artifact in our first batch of bucketing
-        self.aspect_ratio_buckets = torch.round(self.height_buckets / self.width_buckets, decimals=2)
+        self.aspect_ratio_buckets = self.height_buckets / self.width_buckets
 
     def __call__(self, img):
         orig_w, orig_h = img.size
