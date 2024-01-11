9971e2ea65caeb18facbaf74c09270785a1bcb0a
Cory Stephenson <cory.stephenson@databricks.com>
2024-01-11 06:34:53 +0000

    Cleanup logging callback

--- diffusion/callbacks/log_latent_statistics.py
+++ diffusion/callbacks/log_latent_statistics.py
@@ -7,6 +7,7 @@ import math
 from collections import defaultdict
 from typing import Dict
 
+import torch
 from composer import Callback, Logger, State
 
 
@@ -20,14 +21,13 @@ class LogLatentStatistics(Callback):
     def __init__(self, latent_key: str = 'latents'):
         self.latent_key = latent_key
         self.counter = 0
-        self.num_channels = 0
-        self.latent_statistics = defaultdict(lambda: 0.0)
+        self.latent_statistics = defaultdict(float)
 
     def state_dict(self) -> Dict[str, float]:
         return dict(self.latent_statistics)
 
     def load_state_dict(self, state: Dict[str, float]) -> None:
-        latent_statistics = defaultdict(lambda: 0.0)
+        latent_statistics = defaultdict(float)
         for k, v in state.items():
             latent_statistics[k] = v
         self.latent_statistics = latent_statistics
@@ -40,12 +40,8 @@ class LogLatentStatistics(Callback):
         """Compute the standard deviation from the mean and the second moment."""
         return math.sqrt(second_moment - mean**2)
 
-    def eval_start(self, state: State, logger: Logger):
-        # Reset the counter and wipe the latent statistics
-        self.counter = 0
-        self.latent_statistics = defaultdict(lambda: 0.0)
-
-    def eval_batch_end(self, state: State, logger: Logger):
+    def _get_latents(self, state: State) -> torch.Tensor:
+        """Get the latents from the state."""
         # Get outputs from the eval forward pass
         outputs = state.outputs
         assert isinstance(outputs, dict), 'Outputs must be a dict, got {}'.format(type(outputs))
@@ -54,8 +50,16 @@ class LogLatentStatistics(Callback):
             latents = outputs[self.latent_key]  # type: ignore
         else:
             raise ValueError(f'Latent key {self.latent_key} not found in outputs.')
+        return latents
 
-        self.num_channels = latents.shape[1]
+    def eval_start(self, state: State, logger: Logger):
+        # Reset the counter and wipe the latent statistics
+        self.counter = 0
+        self.latent_statistics = defaultdict(lambda: 0.0)
+
+    def eval_batch_end(self, state: State, logger: Logger):
+        # Get the latents
+        latents = self._get_latents(state)
         # Get the global mean and standard deviation
         self.latent_statistics['global_mean'] = self._running_average(self.latent_statistics['global_mean'],
                                                                       latents.mean().item())
@@ -65,7 +69,7 @@ class LogLatentStatistics(Callback):
         other_dims = [i for i in range(latents.ndim) if i != 1]
         channelwise_means = latents.mean(dim=other_dims)
         channelwise_second_moments = (latents**2).mean(dim=other_dims)
-        for i in range(self.num_channels):
+        for i in range(latents.shape[1]):
             self.latent_statistics[f'channel_mean_{i}'] = self._running_average(
                 self.latent_statistics[f'channel_mean_{i}'], channelwise_means[i].item())
             self.latent_statistics[f'channel_second_moment_{i}'] = self._running_average(
@@ -73,11 +77,12 @@ class LogLatentStatistics(Callback):
         self.counter += 1
 
     def eval_end(self, state: State, logger: Logger):
+        latents = self._get_latents(state)
         # Compute the standard deviations from the mean and the mean second moment
         self.latent_statistics['global_std'] = self._calc_std(self.latent_statistics['global_mean'],
                                                               self.latent_statistics['global_second_moment'])
         del self.latent_statistics['global_second_moment']
-        for i in range(self.num_channels):
+        for i in range(latents.shape[1]):
             self.latent_statistics[f'channel_std_{i}'] = self._calc_std(
                 self.latent_statistics[f'channel_mean_{i}'], self.latent_statistics[f'channel_second_moment_{i}'])
             del self.latent_statistics[f'channel_second_moment_{i}']
