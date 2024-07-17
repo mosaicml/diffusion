03a0b6a03978b3db7716f86750178d6a3d784064
Rishab Parthasarathy <56666587+rishab-partha@users.noreply.github.com>
2024-07-17 16:49:31 -0700

    Image generation script for models from here and HF (#158)

--- setup.py
+++ setup.py
@@ -6,21 +6,11 @@
 from setuptools import find_packages, setup
 
 install_requires = [
-    'mosaicml==0.20.1',
-    'mosaicml-streaming==0.7.4',
-    'hydra-core>=1.2',
-    'hydra-colorlog>=1.1.0',
-    'diffusers[torch]==0.26.3',
-    'transformers[torch]==4.38.2',
-    'huggingface_hub==0.21.2',
-    'wandb==0.16.3',
-    'xformers==0.0.23.post1',
-    'triton==2.1.0',
-    'torchmetrics[image]==1.3.1',
-    'lpips==0.1.4',
-    'clean-fid==0.1.35',
-    'clip@git+https://github.com/openai/CLIP.git@a1d071733d7111c9c014f024669f959182114e33',
-    'gradio==4.19.2',
+    'mosaicml==0.20.1', 'mosaicml-streaming==0.7.4', 'hydra-core>=1.2', 'hydra-colorlog>=1.1.0',
+    'diffusers[torch]==0.26.3', 'transformers[torch]==4.38.2', 'huggingface_hub==0.21.2', 'wandb==0.16.3',
+    'xformers==0.0.23.post1', 'triton==2.1.0', 'torchmetrics[image]==1.3.1', 'lpips==0.1.4', 'clean-fid==0.1.35',
+    'clip@git+https://github.com/openai/CLIP.git@a1d071733d7111c9c014f024669f959182114e33', 'gradio==4.19.2',
+    'datasets==2.19.2'
 ]
 
 extras_require = {}
