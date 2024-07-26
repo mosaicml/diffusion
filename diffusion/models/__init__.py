ef74f2b3384db1fd1986d58348ca537511b77076
coryMosaicML <83666378+coryMosaicML@users.noreply.github.com>
2024-07-26 16:26:07 -0700

    MMDiT implementation and text-to-image training with rectified flows (#155)

--- diffusion/models/__init__.py
+++ diffusion/models/__init__.py
@@ -4,7 +4,8 @@
 """Diffusion models."""
 
 from diffusion.models.models import (build_autoencoder, build_diffusers_autoencoder, continuous_pixel_diffusion,
-                                     discrete_pixel_diffusion, stable_diffusion_2, stable_diffusion_xl)
+                                     discrete_pixel_diffusion, stable_diffusion_2, stable_diffusion_xl,
+                                     text_to_image_transformer)
 from diffusion.models.noop import NoOpModel
 from diffusion.models.pixel_diffusion import PixelDiffusion
 from diffusion.models.stable_diffusion import StableDiffusion
@@ -19,4 +20,5 @@ __all__ = [
     'stable_diffusion_2',
     'stable_diffusion_xl',
     'StableDiffusion',
+    'text_to_image_transformer',
 ]
