15e4a4d2cff5ebaa8c7c7f0fcf634f100195c16f
Cory Stephenson <cory.stephenson@databricks.com>
2024-09-18 17:17:36 +0000

    Include mlflow

--- setup.py
+++ setup.py
@@ -6,11 +6,26 @@
 from setuptools import find_packages, setup
 
 install_requires = [
-    'mosaicml==0.24.1', 'mosaicml-streaming==0.8.1', 'hydra-core>=1.2', 'hydra-colorlog>=1.1.0',
-    'diffusers[torch]==0.30.3', 'transformers[torch]==4.44.2', 'huggingface_hub==0.25.0', 'wandb==0.18.1',
-    'xformers==0.0.27post2', 'triton==2.1.0', 'torchmetrics[image]==1.4.2', 'lpips==0.1.4', 'clean-fid==0.1.35',
-    'clip@git+https://github.com/openai/CLIP.git@a1d071733d7111c9c014f024669f959182114e33', 'gradio==4.44.0',
-    'datasets==2.19.2', 'peft==0.12.0', 'sentencepeice',
+    'mosaicml==0.24.1',
+    'mosaicml-streaming==0.8.1',
+    'hydra-core>=1.2',
+    'hydra-colorlog>=1.1.0',
+    'diffusers[torch]==0.30.3',
+    'transformers[torch]==4.44.2',
+    'huggingface-hub[hf_transfer]>=0.23.2',
+    'wandb>=0.18.1',
+    'xformers==0.0.27post2',
+    'triton==2.1.0',
+    'torchmetrics[image]==1.4.2',
+    'lpips==0.1.4',
+    'clean-fid==0.1.35',
+    'clip@git+https://github.com/openai/CLIP.git@a1d071733d7111c9c014f024669f959182114e33',
+    'gradio==4.44.0',
+    'datasets==2.19.2',
+    'peft==0.12.0',
+    'sentencepeice',
+    'mlflow',
+    'pynvml',
 ]
 
 extras_require = {}
